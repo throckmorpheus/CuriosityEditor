31b38069c2ed4d357e4853ffb6b62fbb299d6bde
Throckmorpheus <finnorelsemccormick@gmail.com>
2025-04-09 03:17:34 +0100

    Initial commit

---
+++ NHEditor/NHEditor.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using ImGuiNET;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace NHEditor;
+
+public class NHEditor : ModBehaviour
+{
+	public static NHEditor Instance;
+	public static IModConsole Console => Instance?.ModHelper?.Console;
+
+	public ImGuiOW.API.IImGuiAPI ImGuiAPI;
+
+	public void Awake() => Instance = this;
+
+	public void Start() {
+		Console.Success("NHEditor loaded.");
+
+		new Harmony("Throckmorpheus.NHEditor").PatchAll(Assembly.GetExecutingAssembly());
+
+		ImGuiAPI = ModHelper.Interaction.TryGetModApi<ImGuiOW.API.IImGuiAPI>("Throckmorpheus.ImGuiOW");
+
+		ImGuiAPI.Layout += () => {
+			ImGui.Begin("NHEditor", ImGuiWindowFlags.AlwaysAutoResize);
+			ImGui.Text("Test");
+			ImGui.End();
+		};
+	}
+}
---
+++ NHEditor/util/ModConsoleExtensions.cs
@@ -0,0 +1,31 @@
+using OWML.Common;
+
+namespace NHEditor;
+
+public static class ModConsoleExtensions
+{
+    public static void Message(this IModConsole console, string message, MessageType messageType = MessageType.Message) =>
+        console?.WriteLine(message, messageType);
+
+    public static void Message<T>(this IModConsole console, string message, MessageType messageType = MessageType.Message) =>
+        console?.WriteLine(message, messageType, typeof(T).Name);
+
+    public static void Message<T>(this IModConsole console, T _from, string message, MessageType messageType = MessageType.Message) =>
+        console.Message<T>(message, messageType);
+    
+    public static void Error(this IModConsole console, string message) => console.Message(message, MessageType.Error);
+    public static void Error<T>(this IModConsole console, string message) => console.Message<T>(message, MessageType.Error);
+    public static void Error<T>(this IModConsole console, T _from, string message) => console.Error<T>(message);
+    
+    public static void Success(this IModConsole console, string message) => console.Message(message, MessageType.Success);
+    public static void Success<T>(this IModConsole console, string message) => console.Message<T>(message, MessageType.Success);
+    public static void Success<T>(this IModConsole console, T _from, string message) => console.Success<T>(message);
+    
+    public static void Debug(this IModConsole console, string message) => console.Message(message, MessageType.Debug);
+    public static void Debug<T>(this IModConsole console, string message) => console.Message<T>(message, MessageType.Debug);
+    public static void Debug<T>(this IModConsole console, T _from, string message) => console.Debug<T>(message);
+    
+    public static void Info(this IModConsole console, string message) => console.Message(message, MessageType.Info);
+    public static void Info<T>(this IModConsole console, string message) => console.Message<T>(message, MessageType.Info);
+    public static void Info<T>(this IModConsole console, T _from, string message) => console.Info<T>(message);
+}
